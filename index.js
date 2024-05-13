ac82fdfa450bf3f4db7c02cdf5d5646f41ce1e52
sidmarvictorino <sidmarvictorino@gmail.com>
2024-05-13 17:43:10 -0300

    alteração no comparador

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector('.main-min').value)
     const max = Math.floor(document.querySelector('.main-max').value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (min > max) {
 
-    alert(result)
+        alert("O valor minimo tem que ser MAIOR que o valor maximo")
 
+
+    }
+    else {
+
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        alert(result)
+
+    }
 }
+
+
+
 
